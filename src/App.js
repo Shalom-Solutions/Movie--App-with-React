690620f888044056e1c7299f6f82fe1e37f2d820
Shalom-Solutions <omogoyejuwon@gmail.com>
2023-04-16 19:54:15 +0100

    Build a movie app with react, sourced data from TMDB api and added css styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Movie from './components/Movie';
 
+const FEATURED_API = "https://api.themoviedb.org/3/trending/all/day?api_key=92579eaeaad24968effaadd0d340af1d"
+
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=92579eaeaad24968effaadd0d340af1d&query=";
+
+ 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+   getMovies(FEATURED_API)
+  }, []);
+
+  const getMovies = (API) =>  {
+     fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setMovies(data.results)}
+      );
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies (SEARCH_API + searchTerm);
+      // fetch(SEARCH_API + searchTerm)
+      //     .then(res => res.json())
+      //     .then(data => {
+      //       console.log(data);
+      //       setMovies(data.results)}
+      //       );
+     setSearchTerm("");
+    }
+    
+  }
+
+  
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <header>
+      <form onSubmit={handleOnSubmit}>
+        <input type='search' 
+        className='search' 
+        placeholder='Search...'  
+        value={searchTerm}
+        onChange={handleOnChange} 
+        />
+      </form>
+  
+  </header>
+    <div className='movie-container'>
+     
+      {
+        movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie}
+        />)
+      }
     </div>
+    </>
+   
   );
 }
 
